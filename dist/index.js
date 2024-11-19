8d0208ab1102f8743cd2a9782d22eab0d6f8b9cc
bertapasamontes <bertapasamontes@gmail.com>
2024-11-19 10:53:59 +0100

    ex.3

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,50 @@
 "use strict";
+//variables
 const btnSiguiente = document.getElementById("btn-siguiente-chiste");
 const placeholderChiste = document.querySelector("#chiste-contanier");
 let chiste = document.getElementById("chiste");
+let valoracion = document.querySelector("#valoracion");
+let reportAcudits = [{
+        joke: "...",
+        score: 1,
+        date: ""
+    }
+];
+console.log(reportAcudits);
+let puntuacion = 0;
+//si cambia la valoración:
+valoracion.addEventListener("change", (event) => {
+    const boton = event.target;
+    puntuacion = parseInt(boton.value || "0", 10);
+    // if(puntuacion != 0){
+    //     reportAcudits.push({
+    //         joke: chiste.textContent || "",
+    //         score: puntuacion,
+    //         date: new Date().toISOString(),
+    //     });
+    // };
+    if (puntuacion != 0) {
+        const chisteExistente = reportAcudits.find(item => item.joke === chiste.textContent);
+        if (chisteExistente) {
+            reportAcudits.pop();
+            reportAcudits.push({
+                joke: chiste.textContent || "",
+                score: puntuacion,
+                date: new Date().toISOString(),
+            });
+        }
+        else {
+            reportAcudits.push({
+                joke: chiste.textContent || "",
+                score: puntuacion,
+                date: new Date().toISOString(),
+            });
+        }
+    }
+    ;
+    console.log("chiste valorado: ", reportAcudits);
+});
+llamandoApi(); //primer chiste
 function llamandoApi() {
     const chisteNuevo = document.createElement("p");
     chisteNuevo.setAttribute("id", "chiste");
@@ -10,13 +53,13 @@ function llamandoApi() {
         headers: { "Accept": "application/json" }
     })
         .then(res => res.json())
-        .then(chisteChistoso => { console.log(chisteChistoso.joke); return chisteChistoso; }) //imprime el chiste por consola y daselo al siguiente then
+        .then(chisteChistoso => { console.log(chisteChistoso.joke); return chisteChistoso; }) //imprime el chiste por consola y daselo al siguiente .then
         .then(respuesta => {
         chisteNuevo.textContent = respuesta.joke;
         if (placeholderChiste != null) {
             if (chiste && placeholderChiste.contains(chiste)) {
-                placeholderChiste.replaceChild(chisteNuevo, chiste); //reemplaza
-                chiste = chisteNuevo; //asignamos el chiste nuevo al viejo para poder reemplazarlo a al siguiente ronda
+                placeholderChiste.replaceChild(chisteNuevo, chiste); //reemplaza chiste viejo por nuevo
+                chiste = chisteNuevo; //asignamos el chiste nuevo al viejo para poder reemplazarlo al siguiente
             }
             else {
                 placeholderChiste.appendChild(chisteNuevo); //si no hay chiste, añadelo
@@ -26,7 +69,23 @@ function llamandoApi() {
 }
 let llamamientoDeApi = new Promise((resolve, reject) => {
     if (btnSiguiente) {
-        btnSiguiente.addEventListener("click", llamandoApi);
+        btnSiguiente.addEventListener("click", () => {
+            llamandoApi();
+            if (chiste.textContent) {
+                //añadimos joke a reportAcudits
+                const chisteExistente = reportAcudits.find(item => item.joke === chiste.textContent && item.score !== 0);
+                if (!chisteExistente) {
+                    reportAcudits.push({
+                        joke: chiste.textContent || "",
+                        score: 0, // Última puntuación seleccionada
+                        date: new Date().toISOString(),
+                    });
+                    console.log("chiste nuevo en el array");
+                }
+                puntuacion = 0;
+            }
+            console.log("siguiente chiste", reportAcudits);
+        });
         resolve();
     }
     else {
